4806e4bb71d1dfc3cc57df1bb1db4f8de3ed4038
PalamakulaTarak12 <palamakulatarak2803@gmail.com>
2025-06-10 16:04:02 -0400

    first commit

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -3,17 +3,18 @@ import { createSlice } from '@reduxjs/toolkit';
 export const CartSlice = createSlice({
   name: 'cart',
   initialState: {
-    items: [], // Initialize items as an empty array
+    items: [],
   },
   reducers: {
     addItem: (state, action) => {
-    
+      // Add product to cart items array
+      state.items.push(action.payload);
     },
     removeItem: (state, action) => {
+      // Implement remove functionality here
     },
     updateQuantity: (state, action) => {
-
-    
+      // Implement update quantity functionality here
     },
   },
 });
